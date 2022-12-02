56ee59c0769e24c9ec878ff24761b2cfcd113387
hli <hli-papermoon@hendriklipka.de>
2022-12-02 09:03:12 +0100

    add file parse utils

---
+++ src/de/hendriklipka/aoc/AocParseUtils.java
@@ -0,0 +1,67 @@
+package de.hendriklipka.aoc;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * User: hli
+ * Date: 02.12.22
+ * Time: 08:04
+ */
+public class AocParseUtils
+{
+    public static List<List<Integer>> getIntegerBlocks(final String dayName) throws IOException
+    {
+        List<String> lines = FileUtils.readLines(new File("data/"+ dayName +".txt"), StandardCharsets.UTF_8);
+
+        List<List<Integer>> blocks = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        blocks.add(current);
+        for (String line : lines)
+        {
+            if (StringUtils.isBlank(line))
+            {
+                current = new ArrayList<>();
+                blocks.add(current);
+                continue;
+            }
+            current.add(Integer.parseInt(line));
+        }
+        return blocks;
+    }
+
+    public static List<List<String>> getStringBlocks(final String dayName) throws IOException
+    {
+        List<String> lines = FileUtils.readLines(new File("data/" + dayName + ".txt"), StandardCharsets.UTF_8);
+
+        List<List<String>> blocks = new ArrayList<>();
+        List<String> current = new ArrayList<>();
+        blocks.add(current);
+        for (String line : lines)
+        {
+            if (StringUtils.isBlank(line))
+            {
+                current = new ArrayList<>();
+                blocks.add(current);
+                continue;
+            }
+            current.add(line);
+        }
+        return blocks;
+    }
+
+
+    public static List<List<String>> getLineWords(final String dayName) throws IOException
+    {
+        List<String> lines = FileUtils.readLines(new File("data/" + dayName + ".txt"), StandardCharsets.UTF_8);
+        return lines.stream().filter(StringUtils::isNotBlank).map(l->Arrays.asList(StringUtils.split(l, " "))).collect(Collectors.toList());
+    }
+}
